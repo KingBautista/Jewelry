6af927a85cea5226308e22a3042bb290baef199c
Ernani Bautista <bautistael23@gmail.com>
2025-10-05 22:30:41 +0800

    updates

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () { return view('scan'); });
+Route::get('/', function () { return view('welcome'); });
 Route::get('/check-shell-exec', function () {
     if (function_exists('shell_exec')) {
         try {   
